4db508394a69d744dc83539b1fc09d17d14533c3
mih <michael.hoppe@fiz-karlsruhe.de>
2014-09-09 16:18:17 +0200

    add more tests

--- larch-server/src/main/java/net/objecthunter/larch/controller/SearchController.java
+++ larch-server/src/main/java/net/objecthunter/larch/controller/SearchController.java
@@ -25,7 +25,6 @@ import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 
-import net.objecthunter.larch.model.Entity.EntityType;
 import net.objecthunter.larch.model.SearchResult;
 import net.objecthunter.larch.service.EntityService;
 import net.objecthunter.larch.service.backend.elasticsearch.ElasticSearchEntityService.EntitiesSearchField;
@@ -35,7 +34,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
